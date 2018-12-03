b497254dfb43bfa66fb0a0f3b57653f9c52320a5
pras75299 <singh.prashantking@gmail.com>
2018-12-03 18:51:57 +0530

    added css and js required to make sections

--- js/app.js
+++ js/app.js
@@ -1,1 +1,10 @@
-console.log("app");
+$('.testimonial_slider').owlCarousel({
+    items:1,
+    margin:10,
+    loop: true,
+    autoplay: true,
+    autoplayHoverPause: true,
+    margin: 10,
+    URLhashListener:true,
+    startPosition: 'URLHash'
+  });
--- js/custom.js
+++ js/custom.js
@@ -11,4 +11,52 @@ $(document).ready(function(){
           
           $(this).removeClass('transition');
       });
+
+      $(".testimonial-carousel").slick({
+        infinite: true,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        autoplay: false,
+        arrows: true,
+        prevArrow: $(".testimonial-carousel-controls .prev"),
+        nextArrow: $(".testimonial-carousel-controls .next")
+      });
+
+      $('.client_SLider').owlCarousel({
+        loop: true,
+        margin: 10,
+        nav: true,
+        navText: [
+          "<i class='fa fa-caret-left'></i>",
+          "<i class='fa fa-caret-right'></i>"
+        ],
+        autoplay: true,
+        autoplayHoverPause: true,
+        responsive: {
+          0: {
+            items: 1
+          },
+          600: {
+            items: 3
+          },
+          1000: {
+            items: 5
+          }
+        }
+      });
+
+      $('.owl-carousel').owlCarousel({
+        loop: true,
+        margin: 10,
+        nav: true,
+        navText: [
+          "<i class='fa fa-arrow-left'></i>",
+          "<i class='fa fa-arrow-right'></i>"
+        ],
+        autoplay: true,
+        autoplayHoverPause: true,
+        
+      });
+
+      
   });
